3a5c208cbd096df0345cd105470a92e23cae76fe
Linus W <mc_lwalker25@mcjvs2.org>
2025-04-01 13:42:04 -0400

    Adding initial foundation to engine

--- source/main.py
+++ source/main.py
@@ -1,4 +1,17 @@
-# Possibly calling it the sheldon game engine
+# DMgine v0.1 - Basic game objects
 
 import os
 import pyfiglet as figlet
+
+class Game:
+    def __init__(self) -> None:
+        pass
+
+class Player():
+    def __init__(self) -> None:
+        pass
+
+class NPC():
+    def __init__(self) -> None:
+        pass
+

a3a6ed630b69ffe387fce458aa7ce926184b65b3
Linus W <mc_lwalker25@mcjvs2.org>
2025-04-02 12:11:39 -0400

    Branching in progress version to alpha

--- source/main.py
+++ source/main.py
@@ -1,17 +1,34 @@
-# DMgine v0.1 - Basic game objects
+# DMgine v0.2 - Test menu and character creation
 
 import os
 import pyfiglet as figlet
 
+Tmp = ''
+
 class Game:
     def __init__(self) -> None:
-        pass
+        players = {}
+
+    def MenuMain():
+        print(figlet.figlet_format("Adventure To Taika", width=200))
+        print("(N)ew game \n(C)haracter creation \n(H)elp")
+        return input("\n>")
+
+    def MakeChar():
+        Player(input("choose your name adventurer:"))
 
 class Player():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, Name) -> None:
+        self.Name = Name 
+        self.Stats = {"str":0, "dex":0, "con":0, "int":0, "wis":0, "cha":0}
 
 class NPC():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, Name) -> None:
+        self.Name = Name 
+        self.Stats = {"str":0, "dex":0, "con":0, "int":0, "wis":0, "cha":0}
 
+
+Tmp = Game.MenuMain()
+
+if Tmp.lower() == "c":
+    Game.MakeChar()

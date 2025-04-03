bbf1cd042785deb43b8e26bca0488c9b676c18ab
Linus W <mc_lwalker25@mcjvs2.org>
2025-04-03 13:58:49 -0400

    v0.3 alpha begin

--- source/main.py
+++ source/main.py
@@ -1,15 +1,17 @@
-# DMgine v0.2 - Test menu and character creation
+# DMgine v0.3 - Traversal, Combat, and Encounters
 
 import os
 import pyfiglet as figlet
 import numpy as np
+import pickle
 
 Tmp = ''
 
 class Game:
     def __init__(self) -> None:
-        players = []
-        map = []
+        self.players = []
+        self.NPCs = []
+        self.map = []
 
 
     def MenuMain(self):
@@ -19,12 +21,15 @@ class Game:
 
     def MakeChar(self):
         while True:
-            Player(input("choose your name adventurer:"))
+            self.players.append(Player(input("choose your name adventurer:")))
             if (input("Do you want to create another character? (Y/N)")).lower() == "n":
                 break
     
+    def MakeNPC(self, Name):
+        self.NPCs.append(NPC(Name))
+    
     def NewGame(self):
-        DM.MakeChar()
+        self.MakeChar()
 
     def LoadGame(self):
         pass
@@ -32,8 +37,29 @@ class Game:
     def SaveGame(self):
         pass
 
+    def RunGame(self): #Main game loop
+        print("(C)ombat (E)ncounter (S)ave (Q)uit \n")
+        Com = input(">") 
+        if Com.lower() == "c":
+            self.Combat()
+        elif Com.lower() == "e":    
+            self.Encounter()
+        elif Com.lower() == "s":
+            self.SaveGame()
+        elif Com.lower() == "q":
+            pass
     def Encounter(self):
         pass
+
+    def Combat(self): #determine order with dexterity check, then call turn() for each player and npc TODO: refactor to use dexterity check
+        while True:
+            for player in self.players:
+                player.turn()
+            for npc in self.NPCs:
+                npc.turn()
+            if (input("Continue combat? (Y/N)")).lower() == "n":
+                break
+        
         
 
 
@@ -41,14 +67,33 @@ class Game:
 class Player():
     def __init__(self, Name) -> None:
         self.Name = Name 
+        self.info = {"HP": 10, "AC": 10}
         self.Stats = {"str":np.random.randint(0,20), "dex":np.random.randint(0,20), "con":np.random.randint(0,20), "int":np.random.randint(0,20), "wis":np.random.randint(0,20), "cha":np.random.randint(0,20)}
         self.inv = {}
         print(self.Stats)
 
+    def turn(self):
+        print("Player turn")
+
+    def move(self):
+        pass
+
 class NPC():
-    def __init__(self, Name) -> None:
+    def __init__(self, Name, Friends, Foes) -> None:
         self.Name = Name 
         self.Stats = {"str":0, "dex":0, "con":0, "int":0, "wis":0, "cha":0}
+        self.inv = {}
+        self.Friends = [Friends]
+        self.Foes = [Foes]
+
+    def turn(self):
+        pass
+
+    def move(self):
+        pass
+    
+    def attack(self):
+        pass
 
 
 #Main Execution block
@@ -57,3 +102,4 @@ Com = DM.MenuMain()
 
 if Com.lower() == "n":
     DM.NewGame()
+    DM.RunGame()

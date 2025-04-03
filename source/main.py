a69b46a4a1ad1161749fb5dcc29de3be219fd2db
Linus W <mc_lwalker25@mcjvs2.org>
2025-04-03 12:12:15 -0400

    Finalizing v0.2

--- source/main.py
+++ source/main.py
@@ -2,25 +2,48 @@
 
 import os
 import pyfiglet as figlet
+import numpy as np
 
 Tmp = ''
 
 class Game:
     def __init__(self) -> None:
-        players = {}
+        players = []
+        map = []
 
-    def MenuMain():
+
+    def MenuMain(self):
         print(figlet.figlet_format("Adventure To Taika", width=200))
         print("(N)ew game \n(C)haracter creation \n(H)elp")
         return input("\n>")
 
-    def MakeChar():
-        Player(input("choose your name adventurer:"))
+    def MakeChar(self):
+        while True:
+            Player(input("choose your name adventurer:"))
+            if (input("Do you want to create another character? (Y/N)")).lower() == "n":
+                break
+    
+    def NewGame(self):
+        DM.MakeChar()
+
+    def LoadGame(self):
+        pass
+
+    def SaveGame(self):
+        pass
+
+    def Encounter(self):
+        pass
+        
+
+
 
 class Player():
     def __init__(self, Name) -> None:
         self.Name = Name 
-        self.Stats = {"str":0, "dex":0, "con":0, "int":0, "wis":0, "cha":0}
+        self.Stats = {"str":np.random.randint(0,20), "dex":np.random.randint(0,20), "con":np.random.randint(0,20), "int":np.random.randint(0,20), "wis":np.random.randint(0,20), "cha":np.random.randint(0,20)}
+        self.inv = {}
+        print(self.Stats)
 
 class NPC():
     def __init__(self, Name) -> None:
@@ -28,7 +51,9 @@ class NPC():
         self.Stats = {"str":0, "dex":0, "con":0, "int":0, "wis":0, "cha":0}
 
 
-Tmp = Game.MenuMain()
+#Main Execution block
+DM = Game()
+Com = DM.MenuMain()
 
-if Tmp.lower() == "c":
-    Game.MakeChar()
+if Com.lower() == "n":
+    DM.NewGame()
